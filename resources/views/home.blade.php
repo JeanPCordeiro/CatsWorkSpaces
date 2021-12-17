c22013da0d0874142b35a2b20deb741e9b0449e6
JeanPCordeiro <jeanpierre.cordeiro@gmail.com>
2021-12-17 16:13:40 +0000

    Set logo and names

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,7 @@
 @extends('adminlte::page')
 
-@section('title', 'AdminLTE')
-
 @section('content_header')
-    <h1 class="m-0 text-dark">Dashboard</h1>
+    <h1 class="m-0 text-dark">CATS WorkSpaces</h1>
 @stop
 
 @section('content')
@@ -11,9 +9,23 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-body">
-                    <p class="mb-0">You are logged in!</p>
+                    Bonjour <B>{{ Auth::user()->name }}</B>, ravi de vous accueillir sur le site <B>CATS WorkSpaces</B> !<BR>
+                    Vous avez une habilitation de niveau <B>{{ Auth::user()->level }}</B><BR>
+                    <I>Le niveau 0 indique que vous êtes en cours d'inscription,<BR>
+                    Le niveau 1 est celui des utilisateurs,<BR>
+                    Le niveau 3 est réservé aux administrateurs.
+                    </I>
                 </div>
             </div>
+
+            <div class="card">
+                <div class="card-body">
+                    <B>CATS WorkSpaces</B> vous permet d'accéder à des environnements complets de développement avec un simple navigateur.<BR>
+                    Pour chaque filière de développement, vous disposerez d'une IDE Microsoft Visual Studio Code, connecté à votre repo GitLab, d'un environnement dédié comprenant tous les langages et outils nécessaires, d'un terminal Linux avec un client Docker complet.
+                </div>
+            </div>
+
+
         </div>
     </div>
 @stop
--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Auth::routes();
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
 
 Route::get('/home', function() {
     return view('home');
