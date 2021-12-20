1475f7aab18625a830eac24d23a94d89df71a336
JeanPCordeiro <jeanpierre.cordeiro@gmail.com>
2021-12-20 08:57:07 +0000

    inst composer v2

---
+++ inst_compo.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+sudo mv composer.phar /usr/bin/composer
